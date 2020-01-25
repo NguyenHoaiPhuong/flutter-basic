412a9e7a56a95050bed61abffe9573b06ebbf85a
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-01-25 16:01:04 +0700

    Scaffold

--- scaffold/lib/main.dart
+++ scaffold/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app_screens/homepage.dart';
+//import 'app_screens/homepage.dart';
 
 // First example of Scaffold
 /*
@@ -65,8 +65,8 @@ class MyFlutterApp extends StatelessWidget {
 }
 */
 
-void main() => runApp(MyFlutterApp());
-
+// Second example of Scaffold - Add logic
+/*void main() => runApp(MyFlutterApp());
 class MyFlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -75,47 +75,57 @@ class MyFlutterApp extends StatelessWidget {
         home: MyHomepage(),
     );
   }
-}
+}*/
 
-
-/*
-void main() => runApp(MyScaffoldWidget());
-
-class MyScaffoldWidget extends StatefulWidget {
+// Third example of Scaffold - State
+void main() => runApp(MyFlutterApp());
+class MyFlutterApp extends StatefulWidget {
   @override
-  _MyScaffoldWidgetState createState() => _MyScaffoldWidgetState();
+  State<StatefulWidget> createState() {
+    return _MyFlutterAppState();
+  }
 }
-
-class _MyScaffoldWidgetState extends State<MyScaffoldWidget> {
+class _MyFlutterAppState extends State<MyFlutterApp> {
   int _count = 0;
-
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Scaffold'),
+    return MaterialApp(
+      title: 'My Flutter App',
+      home: Padding(
+        padding: const EdgeInsets.symmetric(
+            vertical: 25.0, horizontal: 10.0
         ),
-        body: Center(
-          child: Text('You have pressed the button $_count times.'),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          child: Container(
-            height: 50.0,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Scaffold Header"),
           ),
+          body: Center(
+            child: Text(
+              "You have pressed the button $_count times",
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            color: Colors.blue,
+            child: Container(
+              height: 50.0,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => setState(() {
+              _count++;
+            }),
+            tooltip: 'Increment Counter',
+            child: Icon(Icons.add),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => setState(() {
-            _count++;
-          }),
-          tooltip: 'Increment Counter',
-          child: Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-*/
+
--- scaffold/test/widget_test.dart
+++ scaffold/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:scaffold/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyScaffoldWidget());
+    await tester.pumpWidget(MyFlutterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
