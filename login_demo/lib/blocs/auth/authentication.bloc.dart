88c76281f281341cf65c6acc03857fe8de461827
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-04-24 14:14:31 +0700

    Auth

---
+++ login_demo/lib/blocs/auth/authentication.bloc.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_demo/blocs/auth/authentication.dart';
+import 'package:login_demo/repositories/user.repository.dart';
+import 'package:meta/meta.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null);
+
+  @override
+  AuthenticationState get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AppStarted) {
+      final bool hasToken = await userRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationAuthenticated();
+      } else {
+        yield AuthenticationUnauthenticated();
+      }
+    }
+
+    if (event is LoggedIn) {
+      yield AuthenticationLoading();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthenticationLoading();
+      await userRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
+  }
+}
